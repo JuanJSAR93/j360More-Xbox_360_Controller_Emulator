e62f54543b1191b7b2208ebea7b5f11f46ab0098
JuanJSAR93 <juanjsar@users.noreply.github.com>
2026-09-17 00:00:03 -0500

    bump: release version 1.2.0

--- gui_app.py
+++ gui_app.py
@@ -22,7 +22,7 @@ from input_devices import DeviceManager
 from emulator_engine import EmulatorEngine, apply_axis_calibration, apply_trigger_calibration
 from i18n import get_text, get_target_name, SUPPORTED_LANGUAGES
 
-APP_VERSION = "1.1.2"
+APP_VERSION = "1.2.0"
 
 def parse_version(v_str: str) -> tuple:
     if not v_str:
